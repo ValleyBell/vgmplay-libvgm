bcf4dff5e93f6a44c71d3d343053abf0c70b7bc1
Valley Bell <valleybell@gmx.de>
2023-12-31 14:15:46 +0100

    add readme and bump version

--- version.h
+++ version.h
@@ -2,7 +2,7 @@
 #define __VERSION_H__
 
 #define APP_NAME		"VGM Player"
-#define VGMPLAY_VER_STR	"0.51.0"
-#define VGM_VER_STR		"1.71b"
+#define VGMPLAY_VER_STR	"0.51.1"
+#define VGM_VER_STR		"1.72b"
 
 #endif	// __VERSION_H__
